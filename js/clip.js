8624c23f47bfd67dc503f4c70872ebd4fc925d10
maigo999 <roborobotj3@gmail.com>
2018-03-23 15:16:10 +0900

    copy complete
    コピー処理は完成

---
+++ js/clip.js
@@ -0,0 +1,31 @@
+
+const copy = function () {
+    navigator.clipboard.writeText(document.getElementById('copy-text').value)
+      .then(function () {
+            console.log("CopyOK!");
+        }, function () {
+            console.log('CopyNoOK!');
+        });
+};
+
+const copybutton = document.getElementById('click-copy');
+  copybutton.addEventListener('click', function(){
+    copy();
+  }, false);
+
+const paste = function(){
+navigator.clipboard.readText()
+.then(function (text) {
+  document.getElementsByClassName('pastespace').innerHTML = text;　// なんで動かないのおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお
+  console.log(text)
+}, function () {
+  console.log('ペースト失敗');
+    });
+};
+
+const pastebutton = document.getElementById('click-paste');
+  pastebutton.addEventListener('click', function(){
+    paste();
+  }, false);
+
+
